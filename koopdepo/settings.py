51f88c9439cad2f9ef28b814406398d6f08e56b5
maa1379 <mohammad.a.a1379@gmail.com>
2022-01-04 14:07:31 +0330

    update

--- koopdepo/settings.py
+++ koopdepo/settings.py
@@ -171,7 +171,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["koppdepo.darkube.app"]
 
 
 # Application definition
